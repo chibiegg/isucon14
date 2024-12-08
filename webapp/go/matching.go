62f3ecad930c15b5b836b354a77180ec9189faa3
Yosuke Sameshima <sameshima@monoxer.com>
2024-12-08 15:38:24 +0900

    log error

--- webapp/go/matching.go
+++ webapp/go/matching.go
@@ -13,26 +13,26 @@ func runMatching() {
 	ride := &Ride{}
 	if err := db.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at LIMIT 1`); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
-			slog.Info("no rides")
+			slog.Error("no rides", "error", err)
 			return
 		}
-		slog.Info("match error 1")
+		slog.Error("match error 1", "error", err)
 		return
 	}
 
 	matched := &Chair{}
 	if err := db.GetContext(ctx, matched, "SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE AND is_free = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1"); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
-			slog.Info("no chairs")
+			slog.Error("no chairs", "error", err)
 			return
 		} else {
-			slog.Info("match error 2")
+			slog.Error("match error 2", "error", err)
 			return
 		}
 	}
 
 	if _, err := db.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", matched.ID, ride.ID); err != nil {
-		slog.Info("failed to update ride")
+		slog.Error("failed to update ride", "error", err)
 		return
 	}
 
@@ -40,7 +40,7 @@ func runMatching() {
 		ctx,
 		`UPDATE chairs SET is_free = 0 WHERE id = ?`,
 		ride.ChairID); err != nil {
-		slog.Info("failed to update chairs")
+		slog.Error("failed to update chairs", "error", err)
 		return
 	}
 }
